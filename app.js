c404133e375b169a943391a1086184e0e6269f5e
Gwen Guérin <gwenael.guerin@protonmail.com>
2022-11-24 14:16:38 +0100

    test back & front

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tweetsRouter = require('./routes/tweets');
 
 var app = express();
+
+const cors = require('cors')
+app.use(cors({origin:true}))
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/tweets.js
+++ routes/tweets.js
@@ -2,34 +2,27 @@ var express = require('express');
 var router = express.Router();
 require('../models/connection'); // app
 
-const Tweet = require('../models/tweets')
+const Tweet = require('../models/tweets');
 
 router.post('/', (req, res) => {
-    const tweetContent = req.body.tweet
-    if (tweetContent) {
-        const newTweet = new Tweet({
-            tweet: tweetContent
-          });
-     
-          newTweet.save().then(newTweet => {
-            res.json({ result: true, tweet: newTweet });
-          });
-    }
+  const tweetContent = req.body.tweet;
+  if (tweetContent) {
+    const newTweet = new Tweet({
+    // firstname:
+      tweet: tweetContent,
+    });
+
+    newTweet.save().then((newTweet) => {
+      res.json({ result: true, newTweet });
+    });
+  }
 });
 
-// router.get('/', (req, res) => {
-//     Tweet.find({tweet: tweet})
-// })
-
-// router.get('/:token', (req, res) => {
-//   User.findOne({ token: req.params.token }).then(data => {
-//     if (data) {
-//       res.json({ result: true, token: data });
-//     } else {
-//       res.json({ result: false, error: 'User not found' });
-//     }
-//   });
-// });
-
+router.get('/', (req, res) => {
+  Tweet.find().then((data) => {
+    //   console.log(data);
+    res.json(data);
+  });
+});
 
 module.exports = router;
